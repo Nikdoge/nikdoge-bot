3b70cea1678b135799b33027ac695c25470b7c02
Nikdoge <nikdoge@ya.ru>
2023-02-06 01:14:08 +0400

    Remade commands into separate functions

--- nikdoge_discord_bot.py
+++ nikdoge_discord_bot.py
@@ -10,6 +10,7 @@ import check_minecraft_server
 
 FFMPEG_EXECUTABLE = nikdoge.undump_json('nikdoge_bot_settings.json')['FFMPEG_EXECUTABLE']#"C:/Program Files/ffmpeg/bin/ffmpeg.exe"
 FILENAME_LOG = 'nikdoge_bot.log'
+COMMAND_PREFIX = '.'
 
 logging.basicConfig(
     level = logging.INFO,
@@ -27,14 +28,113 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='.', description=description, intents=intents)
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, description=description, intents=intents)
 
 exch = exchange_handler.Exchange()
 nms_checker = check_minecraft_server.Checker("server.nikdoge.ru:25565")
 
+@bot.command(pass_context=True)
+async def nikdogebot(ctx):
+    await help3.invoke(ctx)
+
+@bot.command()
+async def help3(ctx):
+    help_text_en = f"""Nikdoge Bot is only on the start of its way to the bright future, and atm it's pretty dumb
+    {COMMAND_PREFIX}help3 {COMMAND_PREFIX}nikdogebot - help on Nikdoge Bot
+    {COMMAND_PREFIX}woofe - answers "woofe indeed"
+    {COMMAND_PREFIX}rokk - prints rokk ebol
+    {COMMAND_PREFIX}zdravstvuite - says Hello in RYTP style in your voice channel
+    {COMMAND_PREFIX}georgian - prints georgian letters
+    {COMMAND_PREFIX}exchange - georgian exchange"""
+    help_text_ru = f"""Nikdoge Bot только в начале своего пути к светлому будущему, и пока что весьма мало полезен
+    {COMMAND_PREFIX}help3 {COMMAND_PREFIX}nikdogebot - помощь по Nikdoge Bot
+    {COMMAND_PREFIX}woofe - отвечает "woofe indeed"
+    {COMMAND_PREFIX}rokk - печатает весь рокк ебол
+    {COMMAND_PREFIX}zdravstvuite - говорит Здравствуйте в RYTP-манере в твой голосовой канал
+    {COMMAND_PREFIX}georgian - печатает грузинский алфавит
+    {COMMAND_PREFIX}exchange - грузинский обменник (без аргументов печатает помощь по команде)"""
+    embed=discord.Embed(
+        title="Nikdoge Bot help",
+        description=help_text_ru,
+        color=0xFF5733)
+    await ctx.send(embed=embed)
+
+@bot.command(pass_context=True)
+async def ex(ctx):
+    await exchange.invoke(ctx)
+
+@bot.command(pass_context=True)
+async def обмен(ctx):
+    await exchange.invoke(ctx)
+
+@bot.command()
+async def exchange(ctx, *, arg):
+    embed = None
+    response = None
+    send_help = False
+    lang = 'ru'
+    input_string = arg
+    if input_string == []:
+        send_help = True
+    else:
+        response = exch.exchange_handler(input_string)
+    
+    if send_help:
+        if lang == 'ru':
+            description = 'Грузинский обменник\nКоманда ' + exch.help_ru().replace('exchange_handler',COMMAND_PREFIX+'обмен')
+        else:
+            description = 'Georgian exchange\nCommand ' + exch.help().replace('exchange_handler',COMMAND_PREFIX+'exchange')
+        embed=discord.Embed(description=description, color=0xFF5733)
+    
+    await ctx.send(response,embed=embed)
+
+@bot.command()
+async def woofe(ctx):
+    response = 'woofe indeed'
+    await ctx.send(response)
+
+@bot.command()
+async def rokk(ctx):
+        response = '''Р О К К
+Е Б О Л
+М У П Ю
+О В И Ч
+Н И R Е
+Т👠🔑Й'''
+        await ctx.send(response)
+        return
+
+@bot.command()
+async def zdravstvuite(ctx):
+        log.info('zdravstvuite called')
+        response = '''Простите, извините, "здравствуйте" временно не работает; мы обязательно с этим разберёмся!'''
+        await ctx.send(response)
+        return
+        user = message.author
+        # only play music if user is in a voice channel
+        if user.voice != None:
+            voice_channel=user.voice.channel
+            # grab user's voice channel
+            log.info(f"{message.created_at.isoformat()}: {user.nick} in {voice_channel.name}")
+            # create StreamPlayer
+            vc = await voice_channel.connect()
+            vc.play(discord.FFmpegPCMAudio(source='zdravstvuite.mp3', executable=FFMPEG_EXECUTABLE), after=lambda e: log.info('done', e))
+            while vc.is_playing():
+                await asyncio.sleep(1)
+            # disconnect after the player has finished
+            vc.stop()
+            await vc.disconnect()
+        else:
+            await log.info(f"{message.created_at.isoformat()}: {user.nick} not in voice channel")
+
+@bot.command()
+async def georgian(ctx):
+    response = '''Ა а, Ბ б, Გ г, Დ д, Ე э, Ვ в, Ზ з, Თ т, Ი и, Კ к', Ლ л, Მ м, Ნ н, Ო о, Პ п', Ჟ ж, Რ р, Ს с, Ტ т', Უ у, Ფ п, Ქ к, Ღ гх, Ყ кх, Შ ш, Ჩ ч, Ც ц, Ძ дз, Წ ц', Ჭ ч', Ხ хъ, Ჯ дж, Ჰ х'''
+    await ctx.send(response)
+
 
 #https://stackoverflow.com/questions/62069138/how-to-let-a-bot-post-send-a-message-every-5-minutes-or-because-of-another-event
-@tasks.loop(minutes=1.0, count=None)
+@tasks.loop(seconds=30)
 async def my_background_task():
 
     #await bot.wait_until_ready() # ensures cache is loaded
@@ -54,108 +154,6 @@ async def on_ready():
         log.info(f'{guild.id}: {guild.name}')
         
     bot.loop.create_task(my_background_task())
-
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('.help3') or message.content.startswith('.nikdogebot') or message.content.startswith('.nikdoge bot'):
-        help_text_en = """Nikdoge Bot is only on the start of its way to the bright future, and atm it's pretty dumb
-        .help3 .nikdogebot - help on Nikdoge Bot
-        .woofe - answers "woofe indeed"
-        .rokk - prints rokk ebol
-        .zdravstvuite - says Hello in RYTP style in your voice channel
-        .georgian - prints georgian letters
-        .exchange .обмен - georgian exchange"""
-        help_text_ru = """Nikdoge Bot только в начале своего пути к светлому будущему, и пока что весьма мало полезен
-        .help3 .nikdogebot - помощь по Nikdoge Bot
-        .woofe - отвечает "woofe indeed"
-        .rokk - печатает весь рокк ебол
-        .zdravstvuite - говорит Здравствуйте в RYTP-манере в твой голосовой канал
-        .georgian - печатает грузинский алфавит
-        .exchange .обмен - грузинский обменник (без аргументов печатает помощь по команде)"""
-        embed=discord.Embed(
-            title="Nikdoge Bot help",
-            description=help_text_ru,
-            color=0xFF5733)
-        await message.channel.send(embed=embed)
-        return
-
-    if message.content.startswith('.exchange') or message.content.startswith('.обмен'):
-        analyze_list = message.content.split(' ')
-        embed = None
-        response = None
-        send_help = False
-        lang = 'ru' if analyze_list[0] == '.обмен' else 'en'
-        analyze = ' '.join(analyze_list[1:])
-
-        if len(analyze_list) < 2:
-            send_help = True
-        elif 'help' in analyze_list or 'помощь' in analyze_list:
-            send_help = True
-        else:
-            response = exch.exchange_handler(analyze)
-
-        if send_help:
-            if lang == 'ru':
-                description = 'Грузинский обменник\nКоманда ' + exch.help_ru().replace('exchange_handler','.обмен')
-            else:
-                description = 'Georgian exchange\nCommand ' + exch.help().replace('exchange_handler','.exchange')
-            embed=discord.Embed(description=description, color=0xFF5733)
-        
-        await message.channel.send(response,embed=embed)
-        return
-
-    if message.content.startswith('.woofe'):
-        response = 'woofe indeed'
-        await message.channel.send(response)
-        return
-
-    
-
-    if message.content.startswith('.rokk'):
-        response = '''
-    Р О К К
-    Е Б О Л
-    М У П Ю
-    О В И Ч
-    Н И R Е
-    Т👠🔑Й'''
-        await message.channel.send(response)
-        return
-
-    if message.content.startswith('.zdravstvuite'):
-        log.info('zdravstvuite called')
-        response = '''Простите, извините, "здравствуйте" временно не работает; мы обязательно с этим разберёмся!'''
-        await message.channel.send(response)
-        return
-        user = message.author
-        # only play music if user is in a voice channel
-        if user.voice != None:
-            voice_channel=user.voice.channel
-            # grab user's voice channel
-            log.info(f"{message.created_at.isoformat()}: {user.nick} in {voice_channel.name}")
-            # create StreamPlayer
-            vc = await voice_channel.connect()
-            vc.play(discord.FFmpegPCMAudio(source='zdravstvuite.mp3', executable=FFMPEG_EXECUTABLE), after=lambda e: log.info('done', e))
-            while vc.is_playing():
-                await asyncio.sleep(1)
-            # disconnect after the player has finished
-            vc.stop()
-            await vc.disconnect()
-        else:
-            await log.info(f"{message.created_at.isoformat()}: {user.nick} not in voice channel")
-        return
-
-    if message.content.startswith('.georgian'):
-        response = '''Ა а, Ბ б, Გ г, Დ д, Ე э, Ვ в, Ზ з, Თ т, Ი и, Კ к', Ლ л, Მ м, Ნ н, Ო о, Პ п', Ჟ ж, Რ р, Ს с, Ტ т', Უ у, Ფ п, Ქ к, Ღ гх, Ყ кх, Შ ш, Ჩ ч, Ც ц, Ძ дз, Წ ц', Ჭ ч', Ხ хъ, Ჯ дж, Ჰ х'''
-        await message.channel.send(response)
-        return
-
-    #https://discordpy.readthedocs.io/en/latest/faq.html#why-does-on-message-make-my-commands-stop-working
-    await bot.process_commands(message)
 
 
 log.info('Starting Nikdoge Discord bot')
