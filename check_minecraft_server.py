0348798c99ddada97726444d31bf43cfde98c35a
Nikdoge <nikdoge@ya.ru>
2023-02-20 21:16:11 +0400

    Dividing two protocols

--- check_minecraft_server.py
+++ check_minecraft_server.py
@@ -1,6 +1,7 @@
 from mcstatus import JavaServer
 
 #SERVER_ADDRESS = "server.nikdoge.ru:25565"
+USE_QUERY_PROTOCOL = False
 
 class Checker:
     server_address = None
@@ -45,18 +46,26 @@ class Checker:
 
     def refresh_data(self):
         self.server = JavaServer.lookup(self.server_address) #must catch exceptions like ValueError('%r does not appear to be an IPv4 or IPv6 address'), help_command = None
-
-        try:
-            self.query_success = True
-            players_new = set(self._get_players())
-            players_amount_new = len(players_new)
-        except TimeoutError as e:
-            self.query_success = False
+        use_query_protocol = USE_QUERY_PROTOCOL
+        # Try to use query protocol if enabled
+        if use_query_protocol == True:
+            try:
+                print('quering')
+                self.query_success = True
+                players_new = set(self._get_players())
+                players_amount_new = len(players_new)
+            except TimeoutError as e:
+                self.query_success = False
+                use_query_protocol = False
+        # Use status protocol if query disabled or failed
+        if use_query_protocol == False:
+            print('statusing')
             status = self._get_status()
             players_new = set([elem.name for elem in status.players.sample])
             players_amount_new = status.players.online
-
-        if self.query_success:
+        # Analyze data
+        if use_query_protocol:
+            # In query protocol we can trust player list, so we compare it
             if players_new != self.players:
                 self.players_changed = True
                 if len(players_new) >= len(self.players):
@@ -67,19 +76,20 @@ class Checker:
                 self.players_changed = False
                 self.players_amount_grew = False
         else:
+            # In status protocol we can trust only player amount
             if players_amount_new > self.players_amount:
                 self.players_changed = True
                 self.players_amount_grew = True
             else:
-                self.players_changed = False
+                self.players_changed = False #kinda wrong here, it could change
                 self.players_amount_grew = False
-
-
+        # Output result to class object
         self.players_amount = players_amount_new
         self.players = players_new
 
 
     def get_fresh_entering_players_info(self):
+        '''Returning string with amount and list of players if some new player joined'''
         self.refresh_data()
         if self.players_amount_grew:
             return f"{self.players_amount} player(s) on minecraft server: {', '.join(sorted(self.players))}"
@@ -88,8 +98,7 @@ class Checker:
 
 
     def get_fresh_players_info(self):
-        '''
-        Returns string '''
+        '''Returning string with amount and list of players if player list changed'''
         self.refresh_data()
         if self.players_changed:
             return f"{self.players_amount} player(s) on minecraft server: {', '.join(sorted(self.players))}"
